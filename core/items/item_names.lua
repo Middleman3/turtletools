6646898eb12c7f9ed36b81a54a91f87314e092c9
JonBrown <the1jonbrown@gmail.com>
2019-11-26 13:32:43 -0500

    a

--- core/items/item_names.lua
+++
@@ -1,7 +0,0 @@
-wheat = "minecraft:wheat"
-seeds = "minecraft:wheat_seeds"
---Optional
-potato = "minecraft:potato"
-potatoes = "minecraft:potatoes"
-carrot = "minecraft:carrot"
-carrots = "minecraft:carrots"
