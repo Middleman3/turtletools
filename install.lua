d7b04056b35c06225a67da4341017ff833d1da61
JonBrown <the1jonbrown@gmail.com>
2019-11-26 14:13:41 -0500

    a

--- install.lua
+++ install.lua
@@ -1,5 +1,7 @@
 args = {...}
 default_branch = "testing"
+current_dir = shell.dir()
 shell.setDir("/")
 if fs.exists("turtletools") then fs.delete("turtletools") end
 shell.run("github clone Middleman3/turtletools -b" .. (args[1] or default_branch))
+shell.setDir(current_dir)
