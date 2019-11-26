d6137fc35ac04c0c657a653311240bbb882b215b
JonBrown <the1jonbrown@gmail.com>
2019-11-26 14:31:42 -0500

    a

--- install.lua
+++ install.lua
@@ -5,6 +5,7 @@ print(current_dir)
 shell.setDir("/")
 if fs.exists("turtletools") then fs.delete("turtletools") end
 shell.run("github clone Middleman3/turtletools -b" .. (args[1] or default_branch))
+fs.delete("install.lua")
 fs.copy(fs.find("install.lua"), "/")
 print(current_dir)
 shell.setDir(current_dir)
