58039bbf2435a7094f0ba44ba48be2ac76f888b9
JonBrown <the1jonbrown@gmail.com>
2019-11-26 18:45:20 -0500

    a

--- compile.lua
+++ compile.lua
@@ -1,2 +1,4 @@
 os.loadAPI("/turtletools/import.lua")
 import.load_dir("turtletools/core")
+settings.set("_G", _G)
+settings.save("/turtletools/.tt_settings")
--- tt_run.lua
+++ tt_run.lua
@@ -1,7 +1,11 @@
 args = {...}
+compile_path = "/turtletools/compile"
+settings_path = "turtletools/.tt_settings"
+
 if table.getn(args) == 0 then print("Usage: trun <program>") end
-if not shell.run("/turtletools/compile") then error("Compilation failed, reinstall.") end
-print("searching for " .. args[1])
+if not shell.run(compile_path) then error("Compilation failed, reinstall.") end
+if not settings.load(settings_path) then error("Settings could not be loaded.") end
+print("\nSearching for " .. args[1])
 program = "/"..shell.resolve(args[1])
 print(program)
-if not shell.run(program) then error("Execution failed.") end
+if not os.run(program) then error("Execution failed.") end
