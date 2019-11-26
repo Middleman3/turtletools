e3c9bdd1ea6c22bd018ed8b02006611f97a9180b
JonBrown <the1jonbrown@gmail.com>
2019-11-26 17:55:22 -0500

    a

--- install.lua
+++ install.lua
@@ -1,11 +1,14 @@
 args = {...}
 default_branch = "testing"
 settings.set("_tmp_dir", shell.dir())
+settings.save()
 shell.setDir("/")
 if fs.exists("turtletools") then fs.delete("turtletools") end
 shell.run("github clone Middleman3/turtletools -b " .. (args[1] or default_branch))
 fs.delete("install")
 fs.copy("turtletools/install.lua", "install")
 current_dir = settings.get("_tmp_dir")
+settings.unset("_tmp_dir")
+settings.save()
 print(current_dir)
 shell.setDir(current_dir)
