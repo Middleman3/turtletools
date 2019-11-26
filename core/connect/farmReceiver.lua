e720ca6d9006b33e2d588e51263d2b9f6b705cd0
JonBrown <the1jonbrown@gmail.com>
2019-11-26 12:26:43 -0500

    gitignored things

--- core/connect/farmReceiver.lua
+++
@@ -1,7 +0,0 @@
-rednet.open("right")
-print("Waiting for command...")
-id, message = rednet.receive()
-if message == "farm.lua" then
-  shell.run("farm.lua")
-end
-os.reboot()
--- core/connect/startup.lua
+++
@@ -1,9 +0,0 @@
-rednet.open("left")
-print("Waiting for Command...")
-while true do
-  id, message = rednet.receive()
-  if id == 166 then
-    shell.run(message)
-    rednet.send(166, "END")
-  end
-end
