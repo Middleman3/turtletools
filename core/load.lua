b02cdf5ebf35968f0c76c23d64243cb764f8d403
JonBrown <the1jonbrown@gmail.com>
2019-11-26 12:54:40 -0500

    a

--- core/load.lua
+++ core/load.lua
@@ -12,8 +12,9 @@ end
 function load_dir(dir_path)
     paths = fs.list(dir_path)
     for i, path in ipairs(paths) do
-        if fs.isDir(path) then load_dir(path)
-        else load(path) end
+        full_path = dir_path.."/"..path
+        if fs.isDir(full_path) then load_dir(full_path)
+        else load(full_path) end
     end
 end
 
