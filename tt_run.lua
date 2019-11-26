4fef3ac1bda81e5ddd099c382621efca49e81a38
JonBrown <the1jonbrown@gmail.com>
2019-11-26 18:54:50 -0500

    a

--- tt_run.lua
+++ tt_run.lua
@@ -1,11 +1,16 @@
 args = {...}
+rel_prog = args[1]
+table.remove(args)
 compile_path = "/turtletools/compile"
 settings_path = "turtletools/.tt_settings"
 
-if table.getn(args) == 0 then print("Usage: trun <program>") end
+if #args == 0 then print("Usage: trun <program> <args>") end
 if not shell.run(compile_path) then error("Compilation failed, reinstall.") end
 if not settings.load(settings_path) then error("Settings could not be loaded.") end
-print("\nSearching for " .. args[1])
-program = "/"..shell.resolve(args[1])
-print(program)
-if not os.run(program) then error("Execution failed.") end
+
+env = settings.get("_G")
+
+print("\nSearching for " .. rel_prog)
+abs_prog = "/"..shell.resolve(rel_prog)
+print("Running "..abs_prog)
+if not os.run(env, abs_prog, unpack(args)) then error("Execution failed.") end
