5e7e7884ae4ee33a27ead2eba65f5724d1080dcf
JonBrown <the1jonbrown@gmail.com>
2019-11-26 14:06:22 -0500

    a

--- trun.lua
+++ trun.lua
@@ -1,4 +1,4 @@
 args = {...}
 if table.getn(args) == 0 then print("Usage: trun <program>") end
 if not shell.run("/turtletools/compile") then error("Compilation failed, reinstall.") end
-if not shell.run(args[1]) then error("Execution failed.") end
+if not shell.run(shell.resolve(args[1])) then error("Execution failed.") end
