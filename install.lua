354404f46cd79f29d0fb840ecae174d5a3dfbef0
JonBrown <the1jonbrown@gmail.com>
2019-11-26 13:21:27 -0500

    testing branch created

--- install.lua
+++ install.lua
@@ -1,3 +1,5 @@
+args = {...}
+default_branch = "testing"
 shell.setDir("/")
 if fs.exists("turtletools") then fs.delete("turtletools") end
-shell.run("github clone Middleman3/turtletools")
+shell.run("github clone Middleman3/turtletools -b" .. (args[1] or default_branch))
