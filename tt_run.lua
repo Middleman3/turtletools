d9d636a580547a69a0dbca9045795d6a63ad1197
JonBrown <the1jonbrown@gmail.com>
2019-11-26 18:27:27 -0500

    a

--- tt_run.lua
+++ tt_run.lua
@@ -2,6 +2,6 @@ args = {...}
 if table.getn(args) == 0 then print("Usage: trun <program>") end
 if not shell.run("/turtletools/compile") then error("Compilation failed, reinstall.") end
 print("searching for " .. args[1])
-program = fs.find(args[1])[1]
+program = fs.find("*"..args[1])[1]
 print(program)
 if not shell.run(program) then error("Execution failed.") end
