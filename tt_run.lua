354dd109d7d95e7c519cac0476cae58c99ebc665
JonBrown <the1jonbrown@gmail.com>
2019-11-26 19:37:57 -0500

    a

--- tt_run.lua
+++ tt_run.lua
@@ -10,9 +10,10 @@ if  table.getn(args) == 0 then print("Usage: trun <program> <args>") end
 -- Compile
 os.loadAPI("/turtletools/import.lua")
 import.load_dir("turtletools/core")
+print(1)
 learn.going_forward()
 print(extra_stuff)
-
+print(2)
 print("\nSearching for " .. rel_prog)
 abs_prog = "/"..shell.resolve(rel_prog)
 print("Running "..abs_prog)
