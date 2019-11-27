0fa3a970f89497f7be27da4437c7a803969cb21b
JonBrown <the1jonbrown@gmail.com>
2019-11-27 16:27:53 -0500

    a

--- tt_run.lua
+++ tt_run.lua
@@ -4,7 +4,7 @@ table.remove(args)
 COMPILE_PATH = "/turtletools/compile"
 tester = "it still works!"
 
-if  table.getn(args) == 0 then print("Usage: trun <program> <args>") end
+local compile_only = table.getn(args) == 0
 
 -- Compile
 os.loadAPI("/turtletools/import.lua")
@@ -13,7 +13,9 @@ import.load_dir("turtletools/core")
 -- Spill modules
 import.spill(learn)
 
-print("\nSearching for " .. rel_prog)
-abs_prog = "/"..shell.resolve(rel_prog)
-print("Running "..abs_prog)
-if not shell.run(abs_prog, unpack(args)) then error("Execution failed.") end
+if not compile_only then
+    print("\nSearching for " .. rel_prog)
+    abs_prog = "/"..shell.resolve(rel_prog)
+    print("Running "..abs_prog)
+    if not shell.run(abs_prog, unpack(args)) then error("Execution failed.") end
+end
